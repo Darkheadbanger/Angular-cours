9cd729f338dde5ff1c07b3e3f70749af062e3fdf
David Bouhaben <davidbouhaben@yahoo.co.id>
2023-05-17 18:07:29 +0200

    explication angular a comprendre

--- src/app/my-first/my-first/my-first.component.ts
+++ src/app/my-first/my-first/my-first.component.ts
@@ -18,10 +18,38 @@ export class MyFirstComponent {
   }
 }
 
-// - Qu’est-ce qu’un composant Angular
-// - Comment une application Angular démarre
-// regarder les fichier : main.ts , app.ts, index.html
-// - Qu’est-ce qu’un module Angular, appModule
-// - Comment déclarer des templates et des styles
-// repondre a " C'est quoi Angular CLI"
-// C'est quoi les metadonées
+/**
+ *     Qu’est-ce qu’un composant Angular :
+
+    Un composant Angular contrôle une partie de l'écran appelée vue. Chaque application Angular a au moins un composant, le composant racine, généralement appelé AppComponent. 
+     Un composant se compose d'un template HTML, qui définit la vue, et d'une classe TypeScript, qui gère la logique de la vue.
+
+    Qu’est-ce qu’un décorateur de composant :
+
+    Un décorateur de composant, @Component, est une fonction spéciale qui ajoute des métadonnées à une classe de composant.
+     Ces métadonnées indiquent à Angular comment utiliser la classe : en tant que composant, et comment le composant doit être traité.
+
+    Comment une application Angular démarre :
+
+    Une application Angular démarre en exécutant le fichier main.ts, qui initialise l'environnement d'exécution et lance AppModule (le module racine).
+    AppModule déclare AppComponent (le composant racine), qui est chargé dans le index.html à l'emplacement de la balise <app-root>.
+
+    Qu’est-ce qu’un module Angular, AppModule :
+
+    Un module Angular est une classe décorée avec @NgModule. Les modules sont utilisés pour regrouper des fonctionnalités connexes et fournir un cadre de travail organisé.
+    AppModule est généralement le module racine, qui importe les fonctionnalités nécessaires pour lancer l'application.
+
+    Comment déclarer des templates et des styles :
+
+    Les templates et les styles sont déclarés dans le décorateur de composant. Le template est défini par la propriété template ou templateUrl, et les styles sont définis par la propriété styles ou styleUrls.
+
+    C'est quoi Angular CLI :
+
+    Angular CLI (Command Line Interface) est un outil en ligne de commande qui facilite la création et la gestion des applications Angular.
+    Il permet de générer des squelettes d'applications, de composants, de modules, de services, etc., et de gérer le processus de build et de test.
+
+    C'est quoi les métadonnées :
+
+    Les métadonnées sont des informations qui décrivent la classe à laquelle elles sont associées. En Angular, les métadonnées indiquent à Angular comment traiter une classe.
+    Par exemple, si une classe est décorée avec @Component, les métadonnées indiquent qu'il s'agit d'un composant et fournissent des informations supplémentaires, comme quel template et quels styles utiliser pour ce composant.
+ */
